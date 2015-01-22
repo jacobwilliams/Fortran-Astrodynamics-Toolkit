71d17a30214ffcbcea0c7ebe8ab2dcb3a001d02c
jacobwilliams <jacobwilliams@users.noreply.github.com>
2015-01-21 23:31:40 -0600

    remove tabs

--- src/time_module.f90
+++ src/time_module.f90
@@ -32,28 +32,28 @@
 !    julian_day
 !
 !  DESCRIPTION
-!	Returns the Julian day number (i.e., the Julian date at Greenwich noon) 
-!		on the specified YEAR, MONTH, and DAY.
-!	Valid for any Gregorian calendar date producing a 
-!		Julian date greater than zero.
+!    Returns the Julian day number (i.e., the Julian date at Greenwich noon) 
+!        on the specified YEAR, MONTH, and DAY.
+!    Valid for any Gregorian calendar date producing a 
+!        Julian date greater than zero.
 !
 !  NOTES
 !    http://aa.usno.navy.mil/faq/docs/JD_Formula.php
 !
 !  SOURCE
 
-	pure integer function julian_day(y,m,d)
-	
-	implicit none
-	
-	integer,intent(in) :: y        ! year (YYYY)
-	integer,intent(in) :: m        ! month (MM)
-	integer,intent(in) :: d        ! day (DD)
-	
-	julian_day = d-32075+1461*(y+4800+(m-14)/12)/4+367*&
-		 		(m-2-(m-14)/12*12)/12-3*((y+4900+(m-14)/12)/100)/4
-	
-	end function julian_day
+    pure integer function julian_day(y,m,d)
+    
+    implicit none
+    
+    integer,intent(in) :: y        ! year (YYYY)
+    integer,intent(in) :: m        ! month (MM)
+    integer,intent(in) :: d        ! day (DD)
+    
+    julian_day = d-32075+1461*(y+4800+(m-14)/12)/4+367*&
+                 (m-2-(m-14)/12*12)/12-3*((y+4900+(m-14)/12)/100)/4
+    
+    end function julian_day
 !*****************************************************************************************
     
 !*****************************************************************************************
@@ -63,16 +63,16 @@
 !    julian_date
 !
 !  DESCRIPTION
-!	Returns the Julian date for the specified YEAR, MONTH, DAY, HR, MIN, SEC.
-!	Valid for any Gregorian calendar date producing a 
-!		Julian date greater than zero.
+!    Returns the Julian date for the specified YEAR, MONTH, DAY, HR, MIN, SEC.
+!    Valid for any Gregorian calendar date producing a 
+!        Julian date greater than zero.
 !
 !  AUTHOR
 !    Jacob Williams : 1/21/2015
 !
 !  SOURCE
 
-	function julian_date(y,m,d,hour,minute,second)
+    function julian_date(y,m,d,hour,minute,second)
    
     implicit none
     
@@ -88,7 +88,7 @@
                     minute/1440.0_wp + &
                     second/86400.0_wp
                                 
-	end function julian_date
+    end function julian_date
 !*****************************************************************************************
     
 !*****************************************************************************************
@@ -98,27 +98,27 @@
 !    time_module_test
 !
 !  DESCRIPTION
-!	Test routine for the Julian date routines.
+!    Test routine for the Julian date routines.
 !
 !  AUTHOR
 !    Jacob Williams : 1/21/2015
 !
 !  SOURCE
 
-	subroutine time_module_test()
+    subroutine time_module_test()
 
-	implicit none
-	
+    implicit none
+    
     write(*,*) ''
     write(*,*) '---------------'
     write(*,*) ' time_module_test'
     write(*,*) '---------------'
     write(*,*) ''
 
-	! JD = 2451545.0
-	write(*,*) julian_date(2000,1,1,12,0,0)
+    ! JD = 2451545.0
+    write(*,*) julian_date(2000,1,1,12,0,0)
 
-	end subroutine time_module_test
+    end subroutine time_module_test
 !*****************************************************************************************
        
 !*****************************************************************************************
