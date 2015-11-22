af0693aef5cc076eef24461b5032b24b4e0dd234
Jacob Williams <jacobwilliams@users.noreply.github.com>
2015-11-22 00:37:11 -0600

    added new CRTBP routines an a unit test

--- src/crtbp_module.f90
+++ src/crtbp_module.f90
@@ -15,6 +15,7 @@
     
     public :: compute_crtpb_parameter
     public :: crtbp_derivs
+    public :: normalize_state, unnormalize_state
     public :: crtbp_test
     
     contains
@@ -37,16 +38,78 @@
     real(wp)            :: mu   !! CRTBP parameter \(\mu\)
     
     mu = mu2 / (mu1 + mu2)
-    
-    !other normalized values:
-    !
-    !  m1 = one - mu   ! mass of body 1
-    !  m2 = mu         ! mass of body 2
-    !
-    !  x1 = -mu        ! location of body 1
-    !  x2 = one - mu   ! location of body 2
-    
+        
     end function compute_crtpb_parameter
+!*******************************************************************************
+
+!*******************************************************************************
+!>
+!  Convert state in km, km/s units to normalized CRTBP state.
+
+    subroutine normalize_state(x_unnormalized,mu1,mu2,d12,x_normalized)
+    
+    implicit none
+    
+    real(wp),dimension(6),intent(in)  :: x_unnormalized !! unnormalized state w.r.t. barycenter [km,km/s]
+    real(wp),intent(in)               :: mu1            !! grav. param. of body 1 \( [\mathrm{km}^3/\mathrm{s}^2] \)
+    real(wp),intent(in)               :: mu2            !! grav. param. of body 2 \( [\mathrm{km}^3/\mathrm{s}^2] \)
+    real(wp),intent(in)               :: d12            !! distance between body 1 and body 2 [km]
+    real(wp),dimension(6),intent(out) :: x_normalized   !! CRTBP normalized state
+    
+    real(wp) :: mu,tp,n,tu,du
+    
+	n = sqrt((mu1+mu2)/d12**3)  !mean motion (rad/sec)
+	
+	tu = one/n  !time unit
+	du = d12    !distance unit
+	
+    x_normalized(1:3) = x_unnormalized(1:3) / du       !scale distance
+    x_normalized(4:6) = x_unnormalized(4:6) / (du/tu)  !scale velocity
+    
+    end subroutine normalize_state
+!*******************************************************************************
+  
+!*******************************************************************************
+!>
+!  Convert normalized CRTBP state to km, km/s units.
+!
+!# Notes:
+!
+! See also: http://www.spaceatdia.org/uploads/mariano/ss1/2012SSLecture3.pdf
+!
+!   m1 = one - mu   ! mass of body 1
+!   m2 = mu         ! mass of body 2
+!  
+!   x1 = -mu        ! location of body 1
+!   x2 = one - mu   ! location of body 2
+!  
+!   normalized mass     : 1 MU -> (m1 + m2) kg
+!   normalized position : 1 DU -> d12 km
+!   normalized time     : 1 TU -> 1/n sec
+!                         (2pi TU -> 1 rev -> 2pi/n)
+
+    subroutine unnormalize_state(x_normalized,mu1,mu2,d12,x_unnormalized)
+    
+    implicit none
+    
+    real(wp),dimension(6),intent(in)  :: x_normalized   !! CRTBP normalized state
+    real(wp),intent(in)               :: mu1            !! grav. param. of body 1 \( [\mathrm{km}^3/\mathrm{s}^2] \)
+    real(wp),intent(in)               :: mu2            !! grav. param. of body 2 \( [\mathrm{km}^3/\mathrm{s}^2] \)
+    real(wp),intent(in)               :: d12            !! distance between body 1 and body 2 [km]
+    real(wp),dimension(6),intent(out) :: x_unnormalized !! unnormalized state w.r.t. barycenter [km,km/s]
+    
+    real(wp) :: mu,tp,n,tu,du
+    
+	n = sqrt((mu1+mu2)/d12**3)  !mean motion (rad/sec)
+		
+	tu = one/n  !time unit
+	du = d12    !distance unit
+
+    x_unnormalized(1:3) = x_normalized(1:3) * du       !unscale distance
+    x_unnormalized(4:6) = x_normalized(4:6) * (du/tu)  !unscale velocity
+	
+   
+    end subroutine unnormalize_state
 !*******************************************************************************
 
 !*******************************************************************************
@@ -70,11 +133,11 @@
     v = x(4:6)    ! velocity
 
     !other parameters:
-    omm    = one - mu
-    rb1    = [-mu,zero,zero]   ! location of body 1
-    rb2    = [omm,zero,zero]   ! location of body 2
-    r1     = r - rb1           ! body1 -> sc vector
-    r2     = r - rb2           ! body2 -> sc vector
+    omm = one - mu
+    rb1 = [-mu,zero,zero]   ! location of body 1
+    rb2 = [omm,zero,zero]   ! location of body 2
+    r1  = r - rb1           ! body1 -> sc vector
+    r2  = r - rb2           ! body2 -> sc vector
         
     !compute Jacobi integral:
     ! [ See: http://cosweb1.fau.edu/~jmirelesjames/hw4Notes.pdf ]
@@ -102,19 +165,19 @@
     real(wp) :: r13,r23,omm,c1,c2
     
     !extract variables from x vector:
-    r = x(1:3)    ! position
-    v = x(4:6)    ! velocity
+    r = x(1:3) ! position
+    v = x(4:6) ! velocity
 
     !other parameters:
-    omm    = one - mu
-    rb1    = [-mu,zero,zero]   ! location of body 1
-    rb2    = [omm,zero,zero]   ! location of body 2
-    r1     = r - rb1           ! body1 -> sc vector
-    r2     = r - rb2           ! body2 -> sc vector
-    r13    = norm2(r1)**3
-    r23    = norm2(r2)**3
-    c1     = omm/r13
-    c2     = mu/r23
+    omm = one - mu
+    rb1 = [-mu,zero,zero] ! location of body 1
+    rb2 = [omm,zero,zero] ! location of body 2
+    r1  = r - rb1         ! body1 -> sc vector
+    r2  = r - rb2         ! body2 -> sc vector
+    r13 = norm2(r1)**3
+    r23 = norm2(r2)**3
+    c1  = omm/r13
+    c2  = mu/r23
 
     !normalized gravity from both bodies:
     g(1) = -c1*(r(1) + mu) - c2*(r(1)-one+mu)
@@ -142,10 +205,17 @@
     real(wp),parameter :: mu_moon  = 4902.799108_wp      !! \( \mu_{Moon}  ~ (\mathrm{km}^3/\mathrm{s}^2) \)
     real(wp),parameter :: mu_sun   = 132712440017.987_wp !! \( \mu_{Sun}   ~ (\mathrm{km}^3/\mathrm{s}^2) \)
     
-    real(wp),dimension(6),parameter :: x = [one,one,one,one/ten,one/ten,one/ten] !! sample state (normalized)
+    !< sample state (normalized)
+    !< see: [Celestial Mechanics Notes Set 4: The Circular Restricted Three Body Problem](http://cosweb1.fau.edu/~jmirelesjames/hw4Notes.pdf), p.40.
+    real(wp),dimension(6),parameter :: x = [  0.30910452642073_wp, &
+                                              0.07738174525518_wp, &
+                                              0.0_wp,              &
+                                             -0.72560796964234_wp, &
+                                              1.55464233412773_wp, &
+                                              0.0_wp               ]
 
-    real(wp) :: mu  !! CRTPB parameter
-    real(wp) :: c   !! Jacobi constant
+    real(wp)              :: mu  !! CRTPB parameter
+    real(wp)              :: c   !! Jacobi constant
     real(wp),dimension(6) :: xd  !! derivative vector
     
     write(*,*) ''
@@ -159,9 +229,9 @@
     call crtbp_derivs(mu,x,xd)
     
     write(*,'(A,1X,*(F30.16,1X))') 'Earth-Moon mu:   ', mu
-    write(*,'(A,1X,*(F12.6,1X))')  'sample point x:  ', x
-    write(*,'(A,1X,*(F12.6,1X))')  'sample point c:  ', c
-    write(*,'(A,1X,*(F12.6,1X))')  'sample point xd: ', xd
+    write(*,'(A,1X,*(F12.6,1X))' ) 'Sample point x:  ', x
+    write(*,'(A,1X,*(F12.6,1X))' ) 'Sample point c:  ', c
+    write(*,'(A,1X,*(F12.6,1X))' ) 'Sample point xd: ', xd
     write(*,*) ''
     
     end subroutine crtbp_test
