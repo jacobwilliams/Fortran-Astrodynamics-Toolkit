e1b505b790545e41ab0558ba4c1da89d70f2471f
Jacob Williams <jacobwilliams@users.noreply.github.com>
2022-10-29 17:47:58 -0500

    update tests

--- tests/geodetic_test/geodetic_test.f90
+++ tests/geodetic_test/geodetic_test.f90
@@ -18,9 +18,8 @@ program geodetic_test
     real(wp),parameter :: ax  = 6378137.0_wp    !! Ellipsoid : WGS84 Equatorial radius
     real(wp),parameter :: ay  = 6378137.0_wp    !! Ellipsoid : WGS84 Equatorial radius (same)
     real(wp),parameter :: b   = 6356752.3142_wp !! Ellipsoid : WGS84 Polar radius
-    real(wp),parameter :: tol = 1.0e-12_wp !! tolerance
-
-    real(wp),parameter :: test_tol = 1.0e-9_wp !! tolerance for a failed test
+    real(wp),parameter :: tol = 1.0e-13_wp !! tolerance
+    real(wp),parameter :: test_tol = 1.0e-6_wp !! tolerance for a failed test
 
     real(wp) :: h, tmp, phi, lambda, xi, yi, zi, phi_, lambda_, h_
     real(wp) :: phi_error, lambda_error, h_error
@@ -44,7 +43,7 @@ program geodetic_test
     call cpu_time(tstart)
     do i = 1, n_repeat
 
-        h      = get_random_number(6778.0_wp, 10000.0_wp)
+        h      = get_random_number(1.0_wp, 10000.0_wp)
         lambda = get_random_number(0.0_wp, 360.0_wp) * deg2rad
         phi    = get_random_number(-90.0_wp, 90.0_wp) * deg2rad
 
@@ -57,8 +56,8 @@ program geodetic_test
         err = [phi_error,lambda_error,h_error]
 
         if (any(err > test_tol)) then
-        write(*,*) phi_error, lambda_error, h_error
-        error stop 'FAILURE'
+            write(*,*) phi_error, lambda_error, h_error
+            error stop 'FAILURE'
         end if
         tmp = tmp + norm2(err) !compute something so loop isn't optimized away
 
@@ -71,7 +70,7 @@ program geodetic_test
     call cpu_time(tstart)
     do i = 1, n_repeat
 
-        h      = get_random_number(6778.0_wp, 10000.0_wp)
+        h      = get_random_number(1.0_wp, 10000.0_wp)
         lambda = get_random_number(0.0_wp, 360.0_wp) * deg2rad
         phi    = get_random_number(-90.0_wp, 90.0_wp) * deg2rad
 
@@ -87,9 +86,6 @@ program geodetic_test
     call cpu_time(tstart)
     do i = 1, n_repeat
 
-        !  xi = get_random_number(-10000.0_wp, 10000.0_wp)
-        !  yi = get_random_number(-10000.0_wp, 10000.0_wp)
-        !  zi = get_random_number(-10000.0_wp, 10000.0_wp)
         ! point on or outside the body:
         r = get_random_number(6378137.0_wp, 7378137.0_wp) * &
                 unit([get_random_number(-10000.0_wp, 10000.0_wp),&
@@ -104,7 +100,6 @@ program geodetic_test
     call cpu_time(tstop)
     write(*,'(A30,1X,E30.16,1X,F17.1,1X,A)') 'cartesian_to_geodetic_triaxial : Speed', tmp, n_repeat/(tstop-tstart), 'cases/sec'
 
-
     write(*,*) ''
     write(*,*) ' 1. Oblate Spheroid'
     write(*,*) ''
@@ -114,7 +109,7 @@ program geodetic_test
     call cpu_time(tstart)
     do i = 1, n_repeat
 
-        h      = get_random_number(6778.0_wp, 10000.0_wp)
+        h      = get_random_number(1.0_wp, 10000.0_wp)
         lambda = get_random_number(0.0_wp, 360.0_wp) * deg2rad
         phi    = get_random_number(-90.0_wp, 90.0_wp) * deg2rad
 
@@ -151,7 +146,7 @@ program geodetic_test
     call cpu_time(tstart)
     do i = 1, n_repeat
 
-        h      = get_random_number(6778.0_wp, 10000.0_wp)
+        h      = get_random_number(1.0_wp, 10000.0_wp)
         lambda = get_random_number(0.0_wp, 360.0_wp) * deg2rad
         phi    = get_random_number(-90.0_wp, 90.0_wp) * deg2rad
 
